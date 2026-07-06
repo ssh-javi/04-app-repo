dbe664562ab06377d818466b789800162fec7b87
Javier Martinez <javiermartinezblanco.02@gmail.com>
2026-07-06 12:03:40 -0300

    change: test pipeline

--- main.go
+++ main.go
@@ -6,13 +6,13 @@ import (
 )
 
 func main() {
-	// 🚨 VULNERABILIDAD ADREDE: Token hardcodeado para que salte el escáner de secretos (SAST)
-	// aws_secret_key := "AKIAIOSFODNN7EXAMPLE-FAKE-TOKEN-VAL-D4"
+	// ✅ Código limpio de secretos para pasar el filtro de Trivy
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, `{"status": "secure", "day": 4, "framework": "ArgoCD", "change": "two"}`)
+		fmt.Fprintf(w, `{"status": "secure", "day": 4, "framework": "ArgoCD"}`)
 	})
 
-	fmt.Println("Servidor DevSecOps inicializado en el puerto 8080...", aws_secret_key)
+	// 👇 Quitamos "aws_secret_key" de acá para que Go compile sin errores
+	fmt.Println("Servidor DevSecOps inicializado en el puerto 8080 y verificado seguro...")
 	http.ListenAndServe(":8080", nil)
 }
