46257a871277655afa03987f519679b11290338b
Javier Martinez <javiermartinezblanco.02@gmail.com>
2026-07-06 11:59:30 -0300

    change: test pipeline

--- main.go
+++ main.go
@@ -7,10 +7,10 @@ import (
 
 func main() {
 	// 🚨 VULNERABILIDAD ADREDE: Token hardcodeado para que salte el escáner de secretos (SAST)
-	aws_secret_key := "AKIAIOSFODNN7EXAMPLE-FAKE-TOKEN-VAL-D4"
+	// aws_secret_key := "AKIAIOSFODNN7EXAMPLE-FAKE-TOKEN-VAL-D4"
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, `{"status": "secure", "day": 4, "framework": "ArgoCD", "change": "one"}`)
+		fmt.Fprintf(w, `{"status": "secure", "day": 4, "framework": "ArgoCD", "change": "two"}`)
 	})
 
 	fmt.Println("Servidor DevSecOps inicializado en el puerto 8080...", aws_secret_key)
